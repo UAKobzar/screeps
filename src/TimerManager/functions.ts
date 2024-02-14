5ed6af35f5f8f356e4936c4ccde693cfb61690b0
UAKobzar <vallentine.karpenko@gmail.com>
2024-02-14 16:26:25 +0200

    added defences

--- src/TimerManager/functions.ts
+++ src/TimerManager/functions.ts
@@ -1,5 +1,6 @@
 import { createDefaultBuilder, createDefaultWorker } from "CreepManager/utils";
 import { TimerManager } from "TimerManager";
+import { generateRoad } from "utils/construction";
 
 const Functions: TimerFunctions = {
   spawnCreep: (spanwId: Id<StructureSpawn>, creepName: string) => {
@@ -9,9 +10,10 @@ const Functions: TimerFunctions = {
     }
     const spawn = Game.getObjectById(spanwId);
 
-    let energyCapacity = spawn?.room.energyAvailable ?? 0;
+    let energyCapacity = spawn?.room.energyCapacityAvailable ?? 0;
 
     if (energyCapacity < 300) energyCapacity = 300;
+    if (energyCapacity > 1400) energyCapacity = 1400;
 
     const parts = createDefaultWorker(energyCapacity);
 
@@ -22,6 +24,56 @@ const Functions: TimerFunctions = {
       Memory.timer.tickMemory.spawning = true;
       TimerManager.push("spawnCreep", CREEP_LIFE_TIME, spanwId, creepName);
     }
+  },
+
+  generateDefences: (roomName: string) => {
+    debugger;
+    const room = Game.rooms[roomName];
+    if ((room.controller?.level ?? 0) < 2) return;
+    const spawn = room.find(FIND_MY_SPAWNS)[0];
+    if (spawn === undefined) return;
+
+    const exits = [FIND_EXIT_TOP, FIND_EXIT_RIGHT, FIND_EXIT_BOTTOM, FIND_EXIT_LEFT];
+
+    const rampartPositions: Position[] = [];
+    const towerPositions: Position[] = [];
+
+    for (let exit of exits) {
+      const exitPosition = spawn.pos.findClosestByPath(exit);
+      if (!exitPosition) continue;
+
+      const path = spawn.pos.findPathTo(exitPosition);
+      path.reverse();
+      const towerPositionIndex = path.findIndex(step => step.x < 47 && step.y < 47 && step.x > 2 && step.y > 2);
+      const rampartPositionStep = path[towerPositionIndex - 1];
+      const towerPositionStep = path[towerPositionIndex];
+
+      rampartPositions.push({ x: rampartPositionStep.x, y: rampartPositionStep.y });
+      towerPositions.push(towerPositionStep);
+    }
+    const wallIndexes = [2, 47];
+    for (let y of wallIndexes)
+      for (let x = 3; x < 46; x++) {
+        if (rampartPositions.findIndex(position => position.x === x && position.y === y) === -1)
+          room.createConstructionSite(x, y, STRUCTURE_WALL);
+      }
+
+    for (let x of wallIndexes)
+      for (let y = 3; y < 46; y++) {
+        if (rampartPositions.findIndex(position => position.x === x && position.y === y) === -1)
+          room.createConstructionSite(x, y, STRUCTURE_WALL);
+      }
+
+    for (let rampartPosition of rampartPositions) {
+      room.createConstructionSite(rampartPosition.x, rampartPosition.y, STRUCTURE_RAMPART);
+    }
+
+    for (let towerPosition of towerPositions) {
+      room.createConstructionSite(towerPosition.x, towerPosition.y, STRUCTURE_TOWER);
+      generateRoad(room, new RoomPosition(towerPosition.x, towerPosition.y, room.name));
+    }
+
+    TimerManager.push("generateDefences", 1000, roomName);
   }
 };
 
--- src/utils/position.ts
+++ src/utils/position.ts
@@ -4,3 +4,23 @@ export const comparePostion = <T1 extends { x: number; y: number }, T2 extends {
 ): boolean => {
   return pos1.x == pos2.x && pos1.y == pos2.y;
 };
+
+export const isEmptyPosition = (lookAt: LookAtResult<LookConstant>[], ignoreRoads = false) => {
+  return lookAt.every(
+    a =>
+      a.type === "creep" ||
+      a.type === "powerCreep" ||
+      a.type === "flag" ||
+      (a.type === "structure" && a.structure?.structureType === "road" && ignoreRoads) ||
+      (a.type === "constructionSite" && a.constructionSite?.structureType === "road" && ignoreRoads) ||
+      (a.type === "terrain" && a.terrain !== "wall") //todo rewrite
+  );
+};
+
+export const isStructure = (lookAt: LookAtResult<LookConstant>[], ignoreRoads = false) => {
+  return lookAt.some(
+    a =>
+      (a.type === "structure" && (!ignoreRoads || a.structure?.structureType !== "road")) ||
+      (a.type === "constructionSite" && (!ignoreRoads || a.constructionSite?.structureType !== "road"))
+  );
+};
